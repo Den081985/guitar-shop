76c8b15a57d0bc6d533ecfc99a10bb6aa9fba844
Den081985 <pishnenkod@gmail.com>
2021-12-08 22:01:11 +0300

    add new classes to elements

--- components/Products/products.js
+++ components/Products/products.js
@@ -2,18 +2,21 @@
 /*В классе Products создаем метод render,который добавляет в элементы списка
 деструктурированные свойства элементов массива CATALOG с помощью метода forEach,затем 
 добавляем их в список,помещенный в переменную html,а переменную html в свою очередь
-помещаем в блок products на странице с помощью ROOT_PRODUCTS.innerHTML = html  */
+помещаем в блок products на странице с помощью ROOT_PRODUCTS.innerHTML = html 
+CSS-классы указываем с помощью БЭМ,поэтому в элементе списка используем '__'*/
 class Products {
   render() {
     let htmlCatalog = "";
 
     CATALOG.forEach(({ id, name, price, img }) => {
       htmlCatalog += `
-            <li>
-                <span>${name}</span>
-                <img src="${img}"/>
-                <span>${price}</span>
-                <button>Добавить в корзину</button>
+            <li class = "products-element">
+                <span class= "products-element-name">${name}</span>
+                <img class = "products-element-img" src="${img}"/>
+                <span class = "products-element-price">
+                ${price.toLocaleString()}
+                </span>
+                <button class = "products-element-btn">Добавить в корзину</button>
             </li>
 
         
@@ -21,7 +24,7 @@ class Products {
     });
 
     const html = `
-        <ul>${htmlCatalog}</ul>
+        <ul class = "products-container">${htmlCatalog}</ul>
     
     `;
 

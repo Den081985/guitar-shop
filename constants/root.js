a0b957f57f48eb646c3898df0e21f299ceb35d72
Den081985 <pishnenkod@gmail.com>
2021-12-09 21:55:22 +0300

    add root vars

--- constants/root.js
+++ constants/root.js
@@ -1,3 +1,5 @@
+// В переменные помещаем выборку узлов по Id.Эти переменные будут доступны во всех компонентах
+
 const ROOT_PRODUCTS = document.getElementById("products");
 const ROOT_HEADER = document.getElementById("header");
 const ROOT_SHOPPING = document.getElementById("shopping");

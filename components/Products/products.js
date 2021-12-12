77004eeb4fcaba269a4a344f327a28473e3375ea
Den081985 <pishnenkod@gmail.com>
2021-12-12 22:10:00 +0300

    add localStorageUtil

--- components/Products/products.js
+++ components/Products/products.js
@@ -3,12 +3,43 @@
 деструктурированные свойства элементов массива CATALOG с помощью метода forEach,затем 
 добавляем их в список,помещенный в переменную html,а переменную html в свою очередь
 помещаем в блок products на странице с помощью ROOT_PRODUCTS.innerHTML = html 
-CSS-классы указываем с помощью БЭМ,поэтому в элементе списка используем '__'*/
+Для работы с локальным хранилищем в классе Products добавляем переменные
+ productsStore - для получения элементов из хранилища,activeClass-для изменения стиля
+ кнопки и activeText-для изменения текста кнопки,а так же делаем проверку элемента
+ по индексу на его наличие или отсутствие в хранилище.Добавляем метод  handleSetLocationStorage
+ для обработки события onclick на кнопке   */
 class Products {
+  constructor() {
+    this.classNameActive = "products-element-btn-active";
+    this.labelAdd = "Добавить в корзину";
+    this.labelRemove = "Удалить из корзины";
+  }
+
+  handleSetLocationStorage(element, id) {
+    const { pushProducts, products } = localStorageUtil.putProducts(id);
+
+    if (pushProducts) {
+      element.classList.add(this.classNameActive);
+      element.innerHTML = this.labelRemove;
+    } else {
+      element.classList.remove(this.classNameActive);
+      element.innerHTML = this.labelAdd;
+    }
+  }
   render() {
+    const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = "";
 
     CATALOG.forEach(({ id, name, price, img }) => {
+      let activeClass = "";
+      let activeText = "";
+
+      if (productsStore.indexOf(id) === -1) {
+        activeText = this.labelAdd;
+      } else {
+        activeClass = " " + this.classNameActive;
+        activeText = this.labelRemove;
+      }
       htmlCatalog += `
             <li class = "products-element">
                 <span class= "products-element-name">${name}</span>
@@ -16,7 +47,9 @@ class Products {
                 <span class = "products-element-price">
                 ${price.toLocaleString()}
                 </span>
-                <button class = "products-element-btn">Добавить в корзину</button>
+                <button class = "products-element-btn${activeClass}" onclick = "productsPage.handleSetLocationStorage(this, '${id}');">
+                ${activeText}
+                </button>
             </li>
 
         

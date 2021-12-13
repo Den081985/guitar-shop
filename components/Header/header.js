ddae2e62c96a1d48c56fee099c216e78f0b7cb33
Den081985 <pishnenkod@gmail.com>
2021-12-13 21:36:27 +0300

    add header html-element

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,12 +1,18 @@
 /*Создаем класс для Header в котором вызываем метод render для
 содания структуры блоков,а также вызываем у метода render localStorageUtil.getProducts()
-и получаем количество элементов в хранилище roductsStore.length */
+и получаем количество элементов в хранилище roductsStore.length 
+Добавляем метод handlerOpenShoppingPage() для взаимодействия с классом Shopping
+и помещаем его в обработчик onclick class = "header-counter"*/
 
 class Header {
+  handlerOpenShoppingPage() {
+    shoppingPage.render();
+  }
   render(count) {
     const html = `
         <div class = "header-container">
-            <div class = "header-counter">
+        <h2 class = "header-paragraph">Guitar Shop</h2>
+            <div class = "header-counter" onclick = "headerPage.handlerOpenShoppingPage();">
                 <img src="./image/icons8-задание-выполнено-48.png">${count}
             </div>
         </div>
